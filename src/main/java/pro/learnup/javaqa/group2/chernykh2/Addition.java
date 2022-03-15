8277b1a27ce0c60d67136a53af6e790e7cee4d9c
marinoidka <marinoidka@gmail.com>
2022-03-15 16:40:09 +0400

    light added

--- src/main/java/pro/learnup/javaqa/group2/chernykh2/Addition.java
+++ src/main/java/pro/learnup/javaqa/group2/chernykh2/Addition.java
@@ -4,14 +4,23 @@ import java.util.Arrays;
 
 public class Addition {
 
+    public static boolean isGreenLight = false;
 
     private static int maxSpeed = 7;
 
     public static int droppedOut(int[] speeds) {
         int start = 0;
-        for (int speed : speeds) {
-            if (speed > maxSpeed) {
-                start++;
+        if (isGreenLight == false) {
+            for (int speed : speeds) {
+                if (speed > maxSpeed) {
+                    start++;
+                }
+            }
+        } else {
+            for (int speed : speeds) {
+                if (speed < 0) {
+                    start++;
+                }
             }
         }
         return start;
@@ -19,17 +28,34 @@ public class Addition {
 
     public static int[] speedDroppedOut(int[] speeds) {
         int cnt = 0;
-        for (int speed : speeds) {
-            if (speed > maxSpeed) {
-                cnt++;
+        if (isGreenLight == false) {
+            for (int speed : speeds) {
+                if (speed > maxSpeed) {
+                    cnt++;
+                }
+            }
+        } else {
+            for (int speed : speeds) {
+                if (speed < 0) {
+                    cnt++;
+                }
             }
         }
         int[] second = new int[cnt];
         int i = 0;
-        for (int speed : speeds) {
-            if (speed > maxSpeed) {
-                second[i] = speed;
-                i++;
+        if (isGreenLight == false) {
+            for (int speed : speeds) {
+                if (speed > maxSpeed) {
+                    second[i] = speed;
+                    i++;
+                }
+            }
+        } else {
+            for (int speed : speeds) {
+                if (speed < 0) {
+                    second[i] = speed;
+                    i++;
+                }
             }
         }
         return second;
@@ -37,17 +63,34 @@ public class Addition {
 
     public static int[] speedStayedIn(int[] speeds) {
         int cnt = 0;
-        for (int speed : speeds) {
-            if (speed <= maxSpeed) {
-                cnt++;
+        if (isGreenLight == false) {
+            for (int speed : speeds) {
+                if (speed <= maxSpeed) {
+                    cnt++;
+                }
+            }
+        } else {
+            for (int speed : speeds) {
+                if (speed >= 0) {
+                    cnt++;
+                }
             }
         }
         int[] third = new int[cnt];
         int i = 0;
-        for (int speed : speeds) {
-            if (speed <= maxSpeed) {
-                third[i] = speed;
-                i++;
+        if (isGreenLight == false) {
+            for (int speed : speeds) {
+                if (speed <= maxSpeed) {
+                    third[i] = speed;
+                    i++;
+                }
+            }
+        } else {
+            for (int speed : speeds) {
+                if (speed >= 0) {
+                    third[i] = speed;
+                    i++;
+                }
             }
         }
         return third;
